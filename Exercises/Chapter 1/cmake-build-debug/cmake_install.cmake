d23679401b4f41ae227e714e1555c933bbcbc593
Hamza Wahed <hamzawahed@dal.ca>
2022-07-13 20:17:35 -0300

    add exercise 1-6

--- Exercises/Chapter 1/cmake-build-debug/cmake_install.cmake
+++ Exercises/Chapter 1/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/hamza/git/Books/The C Programming Language/Exercises/Chapter 1
+# Install script for directory: /home/hamza/git/The-C-Programming-Language/Exercises/Chapter 1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/hamza/git/Books/The C Programming Language/Exercises/Chapter 1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/hamza/git/The-C-Programming-Language/Exercises/Chapter 1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
